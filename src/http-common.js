102fff149deb973271c17b451d8415a205c7eb87
Ablov <ablovianney7@gmail.com>
2022-02-16 15:24:45 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
 //  baseURL: "http://127.0.0.1:8000/",
-    baseURL: "http://192.168.1.12:3000/",
+    baseURL: "http://192.168.1.19:3000/",
 //  baseURL: "http://172.20.10.3:3000/",
 
   headers: {
